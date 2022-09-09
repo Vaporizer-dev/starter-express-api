6c8d468d26015abfe101f6ba54b4ba6db6c1656a
Vaporizer <hooman206@gmail.com>
2022-09-09 13:13:47 +0430

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.all('/test', (req, res) => {
+    console.log("Test Page!")
+    res.send('This is a test Page!')
+})
 app.listen(process.env.PORT || 3000)
